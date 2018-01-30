d7cd308fee3ec4ff4f531e4a2197d512b735fdfe
Lukas Seiwald <lseiwald.mmt-b2016@fh-salzburg.ac.at>
2018-01-30 11:53:00 +0100

    login redirect fixed

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
+import { firebaseApp } from '../../config/constants'
 import { signIn } from '../../helpers/auth'
 import './Login.css';
+import {
+    withRouter
+} from 'react-router-dom'
+
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.Login = this.Login.bind(this)
+  }
 
+  Login(){
+    signIn()
+    if(firebaseApp.auth().currentUser){
+      this.props.history.push('../events')
+    }
+  }
 
   render () {
+
     return (
         <div>
           <h1>Login</h1>
-          <button onClick={signIn}>
+          <button onClick={this.Login}>
             Sign In with Google 
           </button>
         </div> 
